4c9aa1ce4c67b51b262c5005e337f649e428ca21
Okirey <111231755+Okirey@users.noreply.github.com>
2022-08-22 14:33:36 +0300

    added axios and openweather API to temperature, city and description elements

---
+++ src/app.js
@@ -0,0 +1,17 @@
+ 
+ function showTemperature(response) {
+ console.log(response.data)
+ let temperatureElement = document.querySelector("#temperature");
+ temperatureElement.innerHTML = Math.round(response.data.main.temp);
+ let cityElement = document.querySelector("#city");
+ cityElement.innerHTML = response.data.name;
+ let descriptionElement = document.querySelector("#description");
+ descriptionElement.innerHTML = response.data.weather[0].main;
+ }
+ 
+ 
+ let apiKey = "dcbbd6fc3a17f5e8c90f576135d1831c";
+
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Budapest&units=metric&appid=${apiKey}`;
+
+ axios.get(apiUrl).then(showTemperature);
