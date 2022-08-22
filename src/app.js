4606e001f36f8638293abc657737b1fb177161f6
Okirey <111231755+Okirey@users.noreply.github.com>
2022-08-22 15:22:43 +0300

    Updated Time element

--- src/app.js
+++ src/app.js
@@ -1,4 +1,21 @@
  
+ function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+       hours = `0${hours}`
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`
+    }
+    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = weekDays[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+ }
+ 
+ 
  function showTemperature(response) {
  console.log(response.data)
  let temperatureElement = document.querySelector("#temperature");
@@ -11,6 +28,8 @@
  humidityElement.innerHTML = response.data.main.humidity;
  let windSpeedElement = document.querySelector("#wind-speed");
  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+ let dateElement = document.querySelector("#date");
+ dateElement.innerHTML = formatDate(response.data.dt * 1000);
  }
  
  
