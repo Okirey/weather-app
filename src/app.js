416a8a7380266f5e43505946993f78c0ed461e37
Okirey <111231755+Okirey@users.noreply.github.com>
2022-08-23 11:06:59 +0300

    Changed weather icon according to API data

--- src/app.js
+++ src/app.js
@@ -19,21 +19,32 @@
  function showTemperature(response) {
  let temperatureElement = document.querySelector("#temperature");
  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+
  let cityElement = document.querySelector("#city");
  cityElement.innerHTML = response.data.name;
+
  let descriptionElement = document.querySelector("#description");
  descriptionElement.innerHTML = response.data.weather[0].main;
+
  let humidityElement = document.querySelector("#humidity");
  humidityElement.innerHTML = response.data.main.humidity;
+
  let windSpeedElement = document.querySelector("#wind-speed");
  windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
  let dateElement = document.querySelector("#date");
  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+ let changingIcon = response.data.weather[0].icon;
+ let iconElement = document.querySelector("#icon");
+ iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${changingIcon}@2x.png`);
+
+ iconElement.setAttribute ("alt", response.data.weather[0].main);
  }
  
  
  let apiKey = "dcbbd6fc3a17f5e8c90f576135d1831c";
-
- let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Budapest&units=metric&appid=${apiKey}`;
+ let city = "Malaga";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
  axios.get(apiUrl).then(showTemperature);
