69df218e086e44bb725303b3b0ecfb28d5efdc63
Okirey <111231755+Okirey@users.noreply.github.com>
2022-08-28 21:50:46 +0300

    Deleted console.log with coordinates

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@
 
  }
  function getForecast(coordinates){
-    console.log(coordinates);
+   
   let apiKey = "dcbbd6fc3a17f5e8c90f576135d1831c"
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
